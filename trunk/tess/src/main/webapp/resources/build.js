2e41998da7401acf734de1c7c7fc0f522c9c6af3
linzp2887@gmail.com <linzp2887@gmail.com@00c68613-f2a2-02ec-ab43-129aee39dda1>
2013-03-24 09:53:03 +0000

    增加js压缩功能

--- trunk/tess/src/main/webapp/resources/build.js
+++ trunk/tess/src/main/webapp/resources/build.js
@@ -1,16 +1,15 @@
-
-require.config({
-	baseUrl :'resources/js-build',
-	paths : {
+//node r.js -o build.js
+({
+	baseUrl:'./js',
+	paths: {
 		jquery : '../../assets/jquery/jquery-1.9.0.min',
 		bootStrap : '../../assets/bootstrap/bootstrap.min',
 		templateConfig : 'template/templateconfig',
 		prettify : '../../assets/prettify/prettify',
 		utils: 'utils/utils'
-	}
-});
-
-
-require(["templateConfig"], function(templateConfig){
-	templateConfig.initialize();
-});
+    },
+    dir:'js-build',
+    modules :[{
+    	name:'template/templateApp'
+    }]
+})
